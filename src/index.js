9251cb81d53a194de494a12d033ce70c9cc745a8
NITO666 <lagun-01@mail.ru>
2022-02-24 04:15:24 +0300

    feat: feat programm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 
 // You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return matrix;
+  let result = []
+    if(typeof(matrix) == 'undefined'){
+      return result
+    }else if(matrix.length == 0 ){
+      return result
+    }else{
+      function fun(arr){
+        if(typeof(arr[0][0])=='object'){
+            for(let i in arr){
+                fun(arr[i])
+            }
+        }else if(typeof(arr[0])=='object'){
+            result = arr.reduce((p,c,i)=>{
+                !(i%2 == 0)? c.reverse().map(e=>p.push(e)): c.map(e=>p.push(e))
+                return p
+            })
+            return result
+        }else{
+            for(let i in arr){
+                result.push(arr[i])
+            }
+            return result
+        }
+        }
+      result = fun(matrix)
+      return result
+    }
 }
+
